8a135e60d4c184e750c974dd3af9f4341e718807
Bart <bart@Mac-mini.local>
2020-08-16 21:47:51 +0200

    add 404 and some style chenges

--- efekt/404.php
+++ efekt/404.php
@@ -4,15 +4,16 @@
 		<section class="not-found-section">
       <div class="container">
         <div class="row">
-          <div class="col-12">
-          <figure class="figure">
+          <div class="col-12 col-md-6 offset-md-3 text-center">        
+          <figure>
             <img src="<?php echo get_template_directory_uri(); ?>/svg/404.svg" alt="TechTir - 404" class="image">
           </figure>
           <article id="post-404" class="article">
-            <header class="header">
-              <h5>
-                <?php _e( 'Chciałeś nas stuningować? <br/ > Lepiej wróć na stronę główną...', 'html5blank' ); ?>
-              </h5>
+            <header class="mb-8">
+              <h1 class="mt-8">- 404 -</h1>
+              <h4>
+                <?php _e( 'Lepiej będzie <br/ > jak wrócisz na stronę główną ;)', 'html5blank' ); ?>
+              </h4>
             </header>
             <a href="<?php echo home_url(); ?>" class="btn btn-primary">
               <?php _e( 'Strona główna', 'html5blank' ); ?>
--- efekt/functions.php
+++ efekt/functions.php
@@ -187,7 +187,7 @@ function html5blank_header_scripts()
         wp_register_script('html5blankscripts', get_template_directory_uri() . '/js/scripts.js', array('jquery'), '1.0.0'); // Custom scripts
         wp_enqueue_script('html5blankscripts'); // Enqueue it!
 
-        wp_enqueue_script( 'wpb_slidepanel', get_template_directory_uri() . '/js/mobile-nav.js', array('jquery'), '1.0.0', true ); // Hamburger
+        wp_enqueue_script( 'wpb_slidepanel', get_template_directory_uri() . '/js/mobile-nav.js', array('jquery'), '1.0.0' ); // Hamburger
     }
 }
 
