861732b13cfc6c2bcdfc943a9cae7ff5559beb35
Bart <bart@Mac-mini.local>
2020-08-23 15:43:29 +0200

    page-header styling

--- efekt/footer.php
+++ efekt/footer.php
@@ -1,23 +1,23 @@
 		<!-- footer -->
 		<footer class="footer bg-black pt-10 mt-10 mt-md-20" role="contentinfo">
 			<div class="container">
-			<div class="row">
-				<div class="col-12 col-md-4">
-					<?php html5blank_nav(); ?>
+				<div class="row">
+					<div class="col-12 col-md-4">
+						<?php html5blank_nav(); ?>
+					</div>
+					<div class="col-12 col-md-4">
+						<?php the_field('footer_contact_data_column_1'); ?>
+					</div>
+					<div class="col-12 col-md-4">
+						<?php the_field('footer_contact_data_column_2'); ?>
+					</div>
 				</div>
-				<div class="col-12 col-md-4">
-				<?php the_field('footer_contact_data_column_1'); ?>
+				<div class="row">
+					<p class="copyright my-4 text-center">
+						&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>. <?php _e('Zaprojektowane', 'b-brave'); ?>
+						<a href="//b-brave.pl" title="b-brave" class="link">b-brave</a>.
+					</p>
 				</div>
-				<div class="col-12 col-md-4">
-					<?php the_field('footer_contact_data_column_2'); ?>
-				</div>
-			</div>
-			<div class="row">
-				<p class="copyright my-4 text-center">
-				&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>. <?php _e('Zaprojektowane', 'b-brave'); ?>
-				<a href="//b-brave.pl" title="b-brave" class="link">b-brave</a>.
-				</p>
-			</div>
 			</div>
 		</footer>
 		<?php wp_footer(); ?>
@@ -28,7 +28,7 @@
 		(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
 		l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
 		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		ga('create', 'UA-XXXXXXXX-XX', 'techtir.pl');
+		ga('create', 'UA-XXXXXXXX-XX', 'efekt-zebrzydowice.pl/');
 		ga('send', 'pageview');
 		</script>
 	</body>
